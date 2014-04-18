b871232c0f53a2a638a78c2c148d9fff025a0197
Jaclyn Tsui <jaclyn.tsui@gmail.com>
2014-04-18 14:43:15 -0700

    Initial Commit

---
+++ My First Project/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  My First Project
+//
+//  Created by Jaclyn Tsui on 4/18/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
