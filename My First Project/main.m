829c5cb7ccfc2d6d391b6a97eef378f59d7137bc
Jaclyn Tsui <jaclyn.tsui@gmail.com>
2014-04-19 15:44:11 -0700

    Changed initial NSLog text

--- My First Project/main.m
+++ My First Project/main.m
@@ -14,7 +14,7 @@ int main(int argc, const char * argv[])
     @autoreleasepool {
         
         // insert code here...
-        NSLog(@"This could be anything!");
+        NSLog(@"testing bada bing bada boom!");
         
     }
     return 0;
