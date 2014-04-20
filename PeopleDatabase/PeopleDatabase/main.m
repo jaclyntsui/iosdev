5e946727f6ec18c77045e0b41fd0462dcf0abbaf
Jaclyn Tsui <jaclyn.tsui@gmail.com>
2014-04-20 11:25:29 -0700

    Enter and print strings

--- PeopleDatabase/PeopleDatabase/main.m
+++ PeopleDatabase/PeopleDatabase/main.m
@@ -15,10 +15,25 @@ int main(int argc, const char * argv[])
         
         //Assigns value of 10
         int x = 10;
+        
         //Telling an object to perform an action by sending it a message
         //Square brackets with method stringWithFormat creates new string from a format string with some parameters
         NSString *myString = [NSString stringWithFormat:@"The variable x stores the number %i", x];
         NSLog(@"%@", myString);
+        
+        NSLog(@"Please enter a word");
+        
+        //Declare a variable named cstring to hold 40 characters
+        char cstring[40];
+        
+        //scanf expects a list of characters from format specifier, %s
+        scanf("%s", cstring);
+        
+        //Create an NSString object from the list of characters that were read in
+        NSString *inputString = [NSString stringWithCString:cstring encoding:1];
+        
+        NSLog(@"You entered the word '%@'", inputString);
+        
         
     }
     return 0;
