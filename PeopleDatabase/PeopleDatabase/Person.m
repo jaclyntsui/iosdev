79f26a01526deacae1ee5d92232fc2cbd154504b
Jaclyn Tsui <jaclyn.tsui@gmail.com>
2014-04-20 13:19:37 -0700

    Input of firstname, lastname and age for one entry only

--- PeopleDatabase/PeopleDatabase/Person.m
+++ PeopleDatabase/PeopleDatabase/Person.m
@@ -17,11 +17,18 @@
     scanf("%s", cstring);
     
     firstName = [NSString stringWithCString:cstring encoding:1];
+    
+    NSLog(@"What is %@'s last name?", firstName);
+    scanf("%s", cstring);
+    lastName = [NSString stringWithCString:cstring encoding:1];
+    
+    NSLog(@"How old is %@ %@?", firstName, lastName);
+    scanf("%i", &age);
 
 }
 
 - (void)printInfo {
-    NSLog(@"First name: %@", firstName);
+    NSLog(@"%@ %@ is %i years old", firstName, lastName, age);
 }
 
 @end
