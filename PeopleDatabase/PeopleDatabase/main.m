2024732af0ce38c4ad1bc282e5f934e495cce77b
Jaclyn Tsui <jaclyn.tsui@gmail.com>
2014-04-20 00:34:59 -0700

    How to declare a variable

--- PeopleDatabase/PeopleDatabase/main.m
+++ PeopleDatabase/PeopleDatabase/main.m
@@ -13,6 +13,7 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
+        //How to store a variable and print it
         NSString *hiWorld = @"Welcome to the big bad world!";
         NSLog(@"%@", hiWorld);
         
