61d5ae3dd475611dc2459062fe97a1df3336891f
Jaclyn Tsui <jaclyn.tsui@gmail.com>
2014-04-19 17:53:05 -0700

    Generating random numbers from 1 to 100

--- My First Project/main.m
+++ My First Project/main.m
@@ -12,9 +12,13 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        int num = 400;
-        // insert code here...
-        NSLog(@"num equals %i", num);
+        int answer = 0;
+        int guess = 0;
+        int turn = 0;
+        
+        //divide randomly generated numbers by 100 gives you 0-99. plus 1 for 1-100
+        answer = arc4random() % 100 + 1;
+        NSLog(@"The random value is %i", answer);
         
     }
     return 0;
