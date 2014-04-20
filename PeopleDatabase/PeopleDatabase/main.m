06b675d85d71e0c6c920d541a6888f090f11bcfa
Jaclyn Tsui <jaclyn.tsui@gmail.com>
2014-04-19 23:59:20 -0700

    Added peopledatabase

---
+++ PeopleDatabase/PeopleDatabase/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  PeopleDatabase
+//
+//  Created by Jaclyn Tsui on 4/19/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        NSString *hiWorld = @"Welcome to the big bad world!";
+        NSLog(@"%@", hiWorld);
+        
+    }
+    return 0;
+}
+
