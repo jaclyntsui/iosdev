1aaa0c18124ae8ff767857b825efc36589971ada
Jaclyn Tsui <jaclyn.tsui@gmail.com>
2014-04-20 19:34:38 -0700

    Do-while loop and for loop to input and store multiple user info

--- PeopleDatabase/PeopleDatabase/main.m
+++ PeopleDatabase/PeopleDatabase/main.m
@@ -13,8 +13,18 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+        char response;
+        
+        //NSMutableArray allows you to add objects to the array
+        NSMutableArray *people = [[NSMutableArray alloc] init];
+        
         //A class is a blueprint to say how an object should look and behave
         
+        
+        
+        //Do-while loop conditions are checked at the end of the loop
+        do{
+            
         //Creates a newPerson object
         //alloc: allocates memory for the object and init: initializes the object that will live at that memory address
         Person *newPerson = [[Person alloc] init];
@@ -22,7 +32,17 @@ int main(int argc, const char * argv[])
         //Call methods
         [newPerson enterInfo];
         [newPerson printInfo];
+        [people addObject:newPerson];
         
+            //Program will now prompt you to ask you if you are done.
+            NSLog(@"Do you want to enter another name? (y/n)");
+            scanf("\n%c", &response);
+        }while(response == 'y');
+        
+        NSLog(@"Number of people in the database: %li", [people count]);
+        for (Person *onePerson in people){
+            [onePerson printInfo];
+        }
         
     }
     return 0;
