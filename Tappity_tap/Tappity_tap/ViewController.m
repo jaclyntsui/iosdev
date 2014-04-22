de86dc23d066756613c27ee1470c496621806ea8
Jaclyn Tsui <jaclyn.tsui@gmail.com>
2014-04-22 16:34:18 -0700

    Audio set up to be called

--- Tappity_tap/Tappity_tap/ViewController.m
+++ Tappity_tap/Tappity_tap/ViewController.m
@@ -15,6 +15,27 @@
 
 @implementation ViewController
 
+- (AVAudioPlayer *)setupAudioPlayerWithFile:(NSString *)file type:(NSString *)type{
+    
+    //AVAudioPlayer needs to know the path in the form of a URL
+    NSString *path = [[NSBundle mainBundle] pathForResource:file ofType:type];
+    NSURL *url = [NSURL fileURLWithPath:path];
+    
+    //NSError object will store any error message if something goes wrong
+    NSError *error;
+    
+    //Set up AVAudioPlayer > pass in URL > error msg if something goes wrong
+    AVAudioPlayer *audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:&error];
+    
+    //if something goes wrong the audioPlayer will be set to nil
+    if(!audioPlayer){
+        NSLog(@"%@", [error description]);
+    }
+    
+    //if all goes well the AVAudioPlayer object will be returned!
+    return audioPlayer;
+}
+
 - (void)viewDidLoad
 {
     //iOS will call viewDidLoad automatically for you when the storyboard and view are loaded
@@ -25,6 +46,10 @@
     
     scoreLabel.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"field_score.png"]];
     timerLabel.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"field_time.png"]];
+    
+    buttonBeep = [self setupAudioPlayerWithFile:@"ButtonTap" type:@"wav"];
+    secondBeep = [self setupAudioPlayerWithFile:@"SecondBeep" type:@"wav"];
+    backgroundMusic = [self setupAudioPlayerWithFile:@"HallOfTheMountainKing" type:@"mp3"];
     
     //Call method will start things off.
     [self setupGame];
