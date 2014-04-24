33415a46bfdac5e4bce6d5d8a53570d0b299522a
Jaclyn Tsui <jaclyn.tsui@gmail.com>
2014-04-23 19:02:18 -0700

    Added superclass overrides

--- Vehicles/Vehicles/Car.m
+++ Vehicles/Vehicles/Car.m
@@ -20,6 +20,30 @@
     return self;
 }
 
+- (NSString *)start
+{
+    return [NSString stringWithFormat:@"Start power source %@.", self.powerSource]
+}
+
+- (NSString *)goForward
+{
+    return [NSString stringWithFormat:@"%@ %@ Then depress gas pedal.", [self start], [self changeGears:@"Forward"]];
+}
+
+- (NSString *)goBackward
+{
+    return [NSString stringWithFormat:@"%@ %@ Check your rear view mirror. Then depress gas pedal.", [self start], [self changeGears:@"Reverse"]];
+}
+
+- (NSString *)stopMoving
+{
+    return [NSString stringWithFormat:@"Depress brake pedal. %@", [self changeGears:@"Park"]];
+}
+
+- (NSString *)makeNoise
+{
+    return @"Beep beep!";
+}
 
 
 @end
