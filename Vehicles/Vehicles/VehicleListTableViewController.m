09782e27b83a1c3bf46cb87c02623f604bcfc2cc
Jaclyn Tsui <jaclyn.tsui@gmail.com>
2014-04-25 00:43:11 -0700

    Deletedd setupVehiclesArray method and vehicles instance

--- Vehicles/Vehicles/VehicleListTableViewController.m
+++ Vehicles/Vehicles/VehicleListTableViewController.m
@@ -10,9 +10,7 @@
 
 #import "VehicleDetailViewController.h"
 #import "Vehicle.h"
-#import "Car.h"
-#import "Motorcycle.h"
-#import "Truck.h"
+
 
 @interface VehicleListTableViewController ()
 @property (nonatomic, strong) NSMutableArray *vehicles;
@@ -20,72 +18,11 @@
 
 @implementation VehicleListTableViewController
 
-#pragma mark - Data setup
--(void)setupVehicleArray
-{
-    //Create a car.
-    Car *mustang = [Car carWithBrandName:@"Ford" modelName:@"Mustang" modelYear:1968
-                             powerSource:@"gas engine" numberOfDoors:2 convertible:YES hatchback:NO sunroof:NO];
-    
-    //Add it to the array
-    [self.vehicles addObject:mustang];
-    
-    //Create another car.
-    Car *outback = [Car carWithBrandName:@"Subaru" modelName:@"Outback" modelYear:1999
-                             powerSource:@"gas engine" numberOfDoors:5 convertible:NO hatchback:YES sunroof:NO];
-    
-    //Add it to the array.
-    [self.vehicles addObject:outback];
-    
-    //Create another car
-    Car *prius = [Car carWithBrandName:@"Toyota" modelName:@"Prius" modelYear:2007
-                           powerSource:@"hybrid engine" numberOfDoors:5 convertible:YES hatchback:YES sunroof:YES];
-    
-    //Add it to the array.
-    [self.vehicles addObject:prius];
-    
-    //Add a motorcycle
-    Motorcycle *harley = [Motorcycle motorcycleWithBrandName:@"Harley-Davidson"
-                                                   modelName:@"Softail" modelYear:1979 engineNoise:@"Vrrrrrrrroooooooooom!"];
-    
-    //Add it to the array.
-    [self.vehicles addObject:harley];
-    
-    //Add another motorcycle
-    Motorcycle *kawasaki = [Motorcycle motorcycleWithBrandName:@"Kawasaki"
-                                                     modelName:@"Ninja" modelYear:2005 engineNoise:@"Neeeeeeeeeeeeeeeeow!"];
-    
-    //Add it to the array
-    [self.vehicles addObject:kawasaki];
-    
-    //Create a truck
-    Truck *silverado = [Truck truckWithBrandName:@"Chevrolet" modelName:@"Silverado"
-                                       modelYear:2011 powerSource:@"gas engine" wheels:4 cargoCapacityCubicFeet:53];
-    
-    [self.vehicles addObject:silverado];
-    
-    //Create another truck
-    Truck *eighteenWheeler = [Truck truckWithBrandName:@"Peterbilt" modelName:@"579"
-                                             modelYear:2013 powerSource:@"diesel engine" wheels:18 cargoCapacityCubicFeet:408];
-    
-    [self.vehicles addObject:eighteenWheeler];
-    
-    //Sort the array by the model year
-    NSSortDescriptor *modelYear = [NSSortDescriptor sortDescriptorWithKey:@"modelYear" ascending:YES];
-    [self.vehicles sortUsingDescriptors:@[modelYear]];
-}
-
 
 #pragma mark - View Lifecycle
 - (void)awakeFromNib
 {
     [super awakeFromNib];
-    
-    //Initialize the vehicle array
-    self.vehicles = [NSMutableArray array];
-    
-    //Call the setup method
-    [self setupVehicleArray];
     
     //Set the title of the View Controller, which will display in the Navigation bar
     self.title = @"Vehicles";
