a19b91e33b520a9a6342ccebe7407d781ee5f1d4
Jaclyn Tsui <jaclyn.tsui@gmail.com>
2014-05-02 07:13:24 +0800

    Created data model

--- ScaryBugs/RWTScaryBugDoc.m
+++ ScaryBugs/RWTScaryBugDoc.m
@@ -7,7 +7,19 @@
 //
 
 #import "RWTScaryBugDoc.h"
+#import "RWTScaryBugData.h"
 
 @implementation RWTScaryBugDoc
+@synthesize data = _data;
+@synthesize thumbImage = _thumbImage;
+@synthesize fullImage = _fullImage;
 
+- (id)initWithTitle:(NSString *)title rating:(float)rating thumbImage:(UIImage *)thumbImage fullImage:(UIImage *)fullImage {
+    if ((self = [super init])) {
+        self.data = [[RWTScaryBugData alloc] initWithTitle:title rating:rating];
+        self.thumbImage = thumbImage;
+        self.fullImage = fullImage;
+    }
+    return self;
+}
 @end
