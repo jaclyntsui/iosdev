980b955c9a7da690dcea3f6ddffc1ed7bb0e4255
Jaclyn Tsui <jaclyn.tsui@gmail.com>
2014-05-28 15:45:58 +0800

    Hooking up UITextField with property in a class

--- ScaryBugs/ScaryBugs/RWTDetailViewController.h
+++ ScaryBugs/ScaryBugs/RWTDetailViewController.h
@@ -13,4 +13,7 @@
 @property (strong, nonatomic) id detailItem;
 
 @property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@property (weak, nonatomic) IBOutlet UITextField *titleField;
+
 @end
